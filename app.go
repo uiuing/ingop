f638e24f32d4c4f1b2494f48bfec8632ca2dcb69
uiuing <uiuing@foxmail.com>
2022-06-03 21:55:30 +0800

    Function added: Check go environment variables
    Signed-off-by: uiuing <uiuing@foxmail.com>

--- app.go
+++ app.go
@@ -1,17 +1,46 @@
 package main
 
 import (
-	"context"
+  "context"
+  "runtime"
+  "os"
+  "strings"
+// 	"github.com/wailsapp/wails/v2/pkg/runtime"
 )
 
 type App struct {
-	ctx context.Context
+  ctx context.Context
 }
 
 func NewApp() *App {
-	return &App{}
+  return &App{}
 }
 
 func (a *App) startup(ctx context.Context) {
-	a.ctx = ctx
+  a.ctx = ctx
+}
+
+func checkSystemType() string {
+  return runtime.GOOS
+}
+
+func (a *App) CheckGoEnvironment() map[string]interface{} {
+  request := make(map[string]interface{})
+
+  _, status := os.LookupEnv("GOROOT")
+  request["status"] = status
+
+  if status {
+    goVersion := runtime.Version()
+    goVersions := strings.Split(goVersion[2:], ".")
+    if goVersions[0] >= "1" && goVersions[1] >= "16" {
+      request["message"] = "success"
+    } else {
+      request["message"] = "errorVersion"
+    }
+  } else {
+    request["message"] = "errorEnvironment"
+  }
+
+  return request
 }
